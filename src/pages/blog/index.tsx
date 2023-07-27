e90581a0909e24830b72bd5622519bdc8831649d
Somesh Kar <somesh.kar@gmail.com>
2023-07-27 19:36:58 +0530

    feat(blog): tagline under hero text

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -6,15 +6,19 @@ import { eudoxus } from "~/utils/fonts";
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
-      <div className="mx-auto max-w-screen-lg text-center">
+      <div className="mx-auto max-w-screen-lg text-center text-[#1F2937]">
         <div
-          className={`${eudoxus.variable} mt-16 font-heading text-7xl font-bold leading-normal text-[#1F2937]`}
+          className={`${eudoxus.variable} mt-16 font-heading text-7xl font-bold leading-normal`}
         >
           Amil Blogs, <span className="text-[#0079FF]">Samachar &</span>{" "}
           Publications all in{" "}
           <span className="underline decoration-[#FFB84C] decoration-8">
             one place
           </span>
+        </div>
+        <div className="mx-auto mt-4 max-w-xl text-lg">
+          Subscribe to our amil blogs, samachar and publications today and stay
+          up to date with all amil related news
         </div>
       </div>
     </Layout>
