44c95d8ef0994831468c8a97d88ebd2442314777
Somesh Kar <somesh.kar@gmail.com>
2023-08-21 13:52:04 +0530

    feat(blog): link blog thumbs to post slug links

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
+import Link from "next/link";
 import { truncate } from "lodash";
 import type { BlogPost } from "~/types/blog";
 
@@ -111,51 +112,55 @@ const BlogPage = ({
           {/* Big landing blog post (only for desktop) */}
           {/* TODO: Hide this on mobile */}
           {blogPosts?.length && blogPosts?.length > 0 && (
-            <div className="cursor-pointer select-none">
-              <div
-                className="h-60 w-11/12 rounded-lg bg-cover bg-center"
-                style={{
-                  backgroundImage: `url("${
-                    blogPosts[0]?.blogDisplayPicture?.url ?? undefined
-                  }")`,
-                }}
-              >
-                <div></div>
-                {/* <img
+            <Link href={`/blog/${blogPosts[0]?.blogSlug}`}>
+              <div className="cursor-pointer rounded-xl p-6 transition-all hover:-translate-y-1 hover:shadow-lg">
+                <div className="mx-auto select-none">
+                  <div
+                    className="h-60 w-full rounded-lg bg-cover bg-center"
+                    style={{
+                      backgroundImage: `url("${
+                        blogPosts[0]?.blogDisplayPicture?.url ?? undefined
+                      }")`,
+                    }}
+                  >
+                    <div></div>
+                    {/* <img
                   className="rounded"
                   alt=""
                   src={blogPosts[0]?.blogDisplayPicture?.url ?? undefined}
                 /> */}
-              </div>
-
-              <div className="mt-8 font-semibold text-[#1F2937] opacity-70">
-                {blogPosts[0]?.author} &middot;{" "}
-                {new Date(
-                  blogPosts[0]?.dateOfBlog as string
-                ).toLocaleDateString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-              </div>
-              <div className="mt-2 text-3xl font-semibold">
-                {blogPosts[0]?.blogTitle}
-              </div>
-              <div className="mt-4 w-11/12">
-                {truncate(blogPosts[0]?.excerpt ?? "", { length: 250 })}
-              </div>
-              {/* <div className="mt-4">Excerpt goes here</div> */}
-              <div className="my-4 flex select-none gap-2">
-                {blogPosts[0]?.blogTags?.map((tag, i) => (
-                  <div
-                    key={i}
-                    className={`rounded-full border border-[#1F2937] px-2 py-1 text-xs`}
-                  >
-                    {tag}
                   </div>
-                ))}
+
+                  <div className="mt-8 font-semibold text-[#1F2937] opacity-70">
+                    {blogPosts[0]?.author} &middot;{" "}
+                    {new Date(
+                      blogPosts[0]?.dateOfBlog as string
+                    ).toLocaleDateString("en-GB", {
+                      day: "numeric",
+                      month: "short",
+                      year: "numeric",
+                    })}
+                  </div>
+                  <div className="mt-2 text-3xl font-semibold">
+                    {blogPosts[0]?.blogTitle}
+                  </div>
+                  <div className="mt-4 w-11/12">
+                    {truncate(blogPosts[0]?.excerpt ?? "", { length: 250 })}
+                  </div>
+                  {/* <div className="mt-4">Excerpt goes here</div> */}
+                  <div className="my-4 flex select-none gap-2">
+                    {blogPosts[0]?.blogTags?.map((tag, i) => (
+                      <div
+                        key={i}
+                        className={`rounded-full border border-[#1F2937] px-2 py-1 text-xs`}
+                      >
+                        {tag}
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
-            </div>
+            </Link>
           )}
 
           {blogPosts?.length && blogPosts?.length > 1 && (
@@ -164,18 +169,21 @@ const BlogPage = ({
               {blogPosts.length &&
                 blogPosts.length >= 1 &&
                 blogPosts.slice(1).map((post, i) => (
-                  <BlogPostThumb
-                    key={i}
-                    orientation="horizontal"
-                    post={{
-                      title: post?.blogTitle ?? "",
-                      author: post?.author ?? "",
-                      date: new Date(post?.dateOfBlog as string) ?? new Date(),
-                      excerpt: truncate(post?.excerpt ?? "", { length: 100 }),
-                      tags: post?.blogTags,
-                      image: post?.blogDisplayPicture?.url ?? "",
-                    }}
-                  />
+                  <Link key={i} href={`/blog/${post?.blogSlug}`}>
+                    <BlogPostThumb
+                      key={i}
+                      orientation="horizontal"
+                      post={{
+                        title: post?.blogTitle ?? "",
+                        author: post?.author ?? "",
+                        date:
+                          new Date(post?.dateOfBlog as string) ?? new Date(),
+                        excerpt: truncate(post?.excerpt ?? "", { length: 100 }),
+                        tags: post?.blogTags,
+                        image: post?.blogDisplayPicture?.url ?? "",
+                      }}
+                    />
+                  </Link>
                 ))}
             </div>
           )}
@@ -193,18 +201,21 @@ const BlogPage = ({
             {blogPosts?.length && blogPosts?.length > 1 && (
               <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6">
                 {blogPosts.map((post, i) => (
-                  <BlogPostThumb
-                    key={i}
-                    orientation="vertical"
-                    post={{
-                      title: post?.blogTitle ?? "",
-                      author: post?.author ?? "",
-                      date: new Date(post?.dateOfBlog as string) ?? new Date(),
-                      excerpt: truncate(post?.excerpt ?? "", { length: 100 }),
-                      tags: post?.blogTags,
-                      image: post?.blogDisplayPicture?.url ?? "",
-                    }}
-                  />
+                  <Link key={i} href={`/blog/${post?.blogSlug}`}>
+                    <BlogPostThumb
+                      key={i}
+                      orientation="vertical"
+                      post={{
+                        title: post?.blogTitle ?? "",
+                        author: post?.author ?? "",
+                        date:
+                          new Date(post?.dateOfBlog as string) ?? new Date(),
+                        excerpt: truncate(post?.excerpt ?? "", { length: 100 }),
+                        tags: post?.blogTags,
+                        image: post?.blogDisplayPicture?.url ?? "",
+                      }}
+                    />
+                  </Link>
                 ))}
               </div>
             )}
