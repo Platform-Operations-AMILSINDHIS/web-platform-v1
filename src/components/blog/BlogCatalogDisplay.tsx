b266042d5a836941013df5788df4ebc6a79ebe9f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-20 20:06:19 +0530

    Added: Prop types to catalog display component

--- src/components/blog/BlogCatalogDisplay.tsx
+++ src/components/blog/BlogCatalogDisplay.tsx
@@ -1,7 +1,31 @@
 import { Box } from "@chakra-ui/react";
 
-const BlogCatalogDisplay = () => {
-  return <Box>Catalog here</Box>;
+interface BlogCatalogProps {
+  blogPosts:
+    | {
+        sys: {
+          id: string;
+        };
+        blogTitle: string;
+        author: string;
+        dateOfBlog: string;
+        excerpt: string;
+        blogTags: string[];
+        blogType: string[];
+        blogDisplayPicture: {
+          url: string | undefined;
+        };
+      }[]
+    | null
+    | undefined;
+  blogType: string;
+}
+
+const BlogCatalogDisplay: React.FC<BlogCatalogProps> = ({
+  blogPosts,
+  blogType,
+}) => {
+  return <Box></Box>;
 };
 
 export default BlogCatalogDisplay;
