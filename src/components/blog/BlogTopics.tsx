586bc7c86019f47de9c30bd960772d8dacf3f49b
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-20 23:26:26 +0530

    Changes

--- src/components/blog/BlogTopics.tsx
+++ src/components/blog/BlogTopics.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Grid, Text } from "@chakra-ui/react";
 
 interface BlogTopicProps {
   blogTags: string[];
@@ -6,7 +6,40 @@ interface BlogTopicProps {
 
 const BlogTopics: React.FC<BlogTopicProps> = ({ blogTags }) => {
   console.log(blogTags);
-  return <Box>hi</Box>;
+  return (
+    <Flex pl={10} pt={5} gap={2} flexDir="column">
+      <Flex my={3} flexDir="column">
+        <Text fontWeight={600} fontSize="xl">
+          Recommended{" "}
+          <span
+            style={{
+              color: "#FF4D00",
+            }}
+          >
+            Topics
+          </span>
+        </Text>
+        <Text fontSize="sm">
+          Choose from the topics you prefer to read from
+        </Text>
+      </Flex>
+      <Grid gap={3} templateColumns="repeat(4,1fr)">
+        {blogTags?.map((tag, index) => {
+          return (
+            <Box
+              fontWeight={500}
+              key={index}
+              bg="gray.100"
+              textAlign="center"
+              className={`rounded-full  px-3 py-2 text-xs`}
+            >
+              {tag}
+            </Box>
+          );
+        })}
+      </Grid>
+    </Flex>
+  );
 };
 
 export default BlogTopics;
