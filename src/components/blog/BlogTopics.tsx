5d3ae634b92bc0e769d66fe302573a882987955a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-20 21:30:40 +0530

    Created: Blog Topics component

---
+++ src/components/blog/BlogTopics.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+interface BlogTopicProps {
+  blogTags: string[];
+}
+
+const BlogTopic: React.FC<BlogTopicProps> = () => {
+  return <Box>hi</Box>;
+};
+
+export default BlogTopic;
