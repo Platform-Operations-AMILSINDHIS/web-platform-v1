9379822415c6e1a47c254b791748b3afb08f2ff0
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-20 21:22:44 +0530

    Added: all blog data in catalog component

--- src/components/blog/BlogCatalogDisplay.tsx
+++ src/components/blog/BlogCatalogDisplay.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
+import { truncate } from "lodash";
 
 interface BlogCatalogProps {
   blogPosts:
@@ -27,11 +28,46 @@ const BlogCatalogDisplay: React.FC<BlogCatalogProps> = ({
 }) => {
   console.log(blogPosts);
   return (
-    <Flex gap={5} flexDir="column">
+    <Flex my={8} gap={8} flexDir="column">
       {blogPosts?.map((blog, index) => {
         return (
-          <Flex key={index}>
-            <Text>{blog?.blogTitle}</Text>
+          <Flex gap={1} flexDir="column" key={index}>
+            <Flex gap={3}>
+              <Text fontSize="sm" color="gray.500" fontWeight={600}>
+                {blog?.author}
+              </Text>{" "}
+              |
+              <Text fontSize="sm" color="gray.500" fontWeight={600}>
+                {new Date(blog?.dateOfBlog).toLocaleDateString("en-GB", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </Text>
+            </Flex>
+            <Text fontWeight={600} fontSize="xl">
+              {blog?.blogTitle}
+            </Text>
+            <Text fontSize="sm" maxW={600}>
+              {truncate(blog?.excerpt ?? "", { length: 200 })}
+            </Text>
+            <Flex mt={2} gap={2}>
+              {blogPosts[0]?.blogTags?.map((tag, i) => (
+                <Box
+                  fontWeight={500}
+                  key={i}
+                  className={`rounded-full border border-[#1F2937] px-3 py-1 text-xs`}
+                >
+                  {tag}
+                </Box>
+              ))}
+              <Box
+                fontWeight={500}
+                className={`rounded-full border border-[#1F2937] px-3 py-1 text-xs`}
+              >
+                {blog?.blogType}
+              </Box>
+            </Flex>
           </Flex>
         );
       })}
