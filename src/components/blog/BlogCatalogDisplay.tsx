d8d1edb5f238142d3d16a159c9568d66cd38e998
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-09-20 20:00:47 +0530

    Created: Blog Catalog Display Component

---
+++ src/components/blog/BlogCatalogDisplay.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+const BlogCatalogDisplay = () => {
+  return <Box>Catalog here</Box>;
+};
+
+export default BlogCatalogDisplay;
--- src/pages/blog/catalog.tsx
+++ src/pages/blog/catalog.tsx
@@ -1,8 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
+import BlogCatalogDisplay from "~/components/blog/blogCatalogDisplay";
+import Layout from "~/components/layout";
+import { satoshi } from "~/utils/fonts";
 
 const CatalogPage: NextPage = () => {
-  return <Box>Catalog Page Here</Box>;
+  return (
+    <Layout title="Catalog">
+      <Box fontFamily={satoshi} px={10}>
+        <Flex my={10}>
+          <Flex gap={1} flexDir="column">
+            <Text fontWeight={600} fontSize={35}>
+              Reading Catalog
+            </Text>
+            <Text maxW={850}>
+              Browse, pick and decide your favorite reading habits, from our
+              ever growing collection of samachar, newsletters and blogs. Stay
+              tuned for more
+            </Text>
+            <BlogCatalogDisplay />
+          </Flex>
+        </Flex>
+      </Box>
+    </Layout>
+  );
 };
 
 export default CatalogPage;
