de76e67cc76bf84d8c8ac133d3d1bf5d4ff65877
Somesh Kar <somesh.kar@gmail.com>
2023-10-13 02:36:05 +0530

    feat(donations page): legacy and founders section

--- src/pages/donations.tsx
+++ src/pages/donations.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from "next";
+import { Spacer } from "@chakra-ui/react";
+
 import Layout from "~/components/layout";
 
 import HeroSection from "~/sections/DonationsPage/HeroSection";
+import LegacyFoundersSection from "~/sections/DonationsPage/LegacyFoundersSection";
 
 const DonationsPage: NextPage = () => {
   return (
     <Layout title="Home">
       <HeroSection />
+
+      <Spacer h="2rem" />
+
+      <LegacyFoundersSection />
     </Layout>
   );
 };
---
+++ src/sections/DonationsPage/LegacyFoundersSection.tsx
@@ -0,0 +1,31 @@
+import { Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+
+const LegacyFoundersSection = () => {
+  return (
+    <Grid
+      p="5rem"
+      gap="1rem"
+      templateColumns="1fr 1fr"
+      backgroundImage="/images/backgrounds/ajrak_bg_legacy.jpg"
+    >
+      <GridItem>
+        <Text color="#FF4D00">Our History and Founders</Text>
+        <Heading fontSize="5xl" fontWeight="semibold">
+          A Legacy of Service & Community Building
+        </Heading>
+      </GridItem>
+      <GridItem>
+        <Text>
+          The Khudabadi Amil Panchayat of Bombay has a storied history that
+          reflects a legacy of unwavering service and a commitment to nurturing
+          a vibrant Amil Sindhi community. Founded on the 24th of February,
+          1952, by a group of ten visionary individuals, the Panchayat emerged
+          as a beacon of hope for underprivileged Sindhis who were displaced
+          from their homeland.
+        </Text>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default LegacyFoundersSection;
