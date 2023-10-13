0e71883799bfbd5a2854914354d9c577a215eeae
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-10-13 08:32:29 +0530

    (chore): Defined Navigation items

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Navbar from "~/components/navbar";
+import Navigation from "./Navigation";
 
 import { satoshi } from "../utils/fonts";
+import { Box } from "@chakra-ui/react";
 
 const Layout: React.FC<{
   title?: string;
@@ -20,17 +21,20 @@ const Layout: React.FC<{
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div
+      <Box
         className={`${satoshi.variable} ${
           maxW && "max-w-screen-xl"
         } mx-auto font-sans`}
       >
-        <Navbar />
+        <Box mx="90px" my={6}>
+          <Navigation />
+        </Box>
         <main className={`mx-auto w-full ${maxW && "px-4 md:px-4"}`}>
           {children}
         </main>
+        xxxx
         <footer></footer>
-      </div>
+      </Box>
     </div>
   );
 };
