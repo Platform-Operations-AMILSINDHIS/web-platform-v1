0a1c31a22d0c0602999c316929281d2b17136fe2
Somesh Kar <somesh.kar@gmail.com>
2023-10-23 00:26:42 +0530

    feat(r2): setup

---
+++ src/pages/api/upload/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import { r2 } from "../../../utils/r2";
+
+export async function POST(request: Request) {
+  try {
+    console.log("Generating an upload URL!");
+
+    const signedUrl = await getSignedUrl(
+      r2,
+      new PutObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: `filename.pdf`,
+      }),
+      { expiresIn: 60 }
+    );
+
+    console.log("Success generating upload URL!");
+
+    return NextResponse.json({ url: signedUrl });
+  } catch (err) {
+    console.log("error");
+  }
+}
---
+++ src/utils/r2.ts
@@ -0,0 +1,12 @@
+import { env } from "../env.mjs";
+
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const r2 = new S3Client({
+  region: "auto",
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: env.R2_ACCESS_KEY_ID,
+    secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+  },
+});
