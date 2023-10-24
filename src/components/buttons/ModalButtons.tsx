846d7ae959774412e5e1b27bd78d2b7957d490e7
akshatsabavat <akshat.sabavat@gmail.com>
2023-10-24 18:08:02 +0530

    feat(Modal CTA) - defined prop types

--- src/components/buttons/ModalButtons.tsx
+++ src/components/buttons/ModalButtons.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@chakra-ui/react";
 
+interface ModalButtonProps {
+  CTAaction: () => void;
+  CTAlabel: string;
+  CTATheme?: boolean;
+}
+
 const ModalButton = () => {
   return <Button></Button>;
 };
