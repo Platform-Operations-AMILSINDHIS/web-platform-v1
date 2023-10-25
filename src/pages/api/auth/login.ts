5bce78ee78d44c63f0eefb9f90be480758e7a751
akshatsabavat <akshat.sabavat@gmail.com>
2023-10-25 23:11:41 +0530

    Created: Extra file to instantiate supabase

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,11 +1,5 @@
-import { env } from "../../../env.mjs";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase_URL = env.SUPABASE_URL;
-const supabase_API_KEY = env.SUPABASE_API_KEY;
-
-const supabase = createClient(supabase_URL, supabase_API_KEY);
+import supabase from "./supabase";
 
 interface SupabaseLoginHandlerRequest extends NextApiRequest {
   body: {
