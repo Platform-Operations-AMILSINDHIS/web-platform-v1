7768ae7e683679b46d4bd2001f9eb4b13df9f59c
akshatsabavat <akshat.sabavat@gmail.com>
2023-10-26 15:53:04 +0530

    feat(catalog) - added layout + route

---
+++ src/pages/blog/catalog.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import Layout from "~/components/layout";
+
+const CatalogPage = () => {
+  return (
+    <Layout title="blog catalog">
+      <Box>Hi</Box>
+    </Layout>
+  );
+};
+
+export default CatalogPage;
