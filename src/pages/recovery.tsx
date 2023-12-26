f2a0c2b857a11f5689c63fc7fda9a7b5ed6d4614
akshatsabavat <akshat.sabavat@gmail.com>
2023-12-26 22:07:09 +0530

    Added : Reset password functionality

--- src/pages/recovery.tsx
+++ src/pages/recovery.tsx
@@ -1,17 +1,45 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
+import axios from "axios";
 import { Form, Formik } from "formik";
+import { useState } from "react";
 import { LabelledInput } from "~/components/forms";
-import { RecoveryPasswordInitialValues } from "~/hooks/useForm";
+import {
+  RecoveryPasswordInitialValues,
+  RecoveryPasswordValues,
+} from "~/hooks/useForm";
 import { RecoveryValidation } from "~/validations/AuthValidations";
 
 const RecoveryPage = () => {
+  const [submitState, setSubmitState] = useState(false);
+
+  const handleSubmit = async (values: RecoveryPasswordValues) => {
+    setSubmitState(true);
+    console.log("hit");
+    const resetPasswordResponse = await axios.post<{ message: string }>(
+      "/api/recovery/resetpwd",
+      {
+        email: values.email,
+        newPassword: values.newPassword,
+      }
+    );
+
+    if (resetPasswordResponse.status === 200) {
+      console.log("success");
+      setSubmitState(false);
+      alert("password reset");
+      window.location.href = "/";
+    } else {
+      console.log("screwed");
+    }
+  };
+
   return (
     <Flex justify="center" align="center" w="full" h="100vh">
       <Flex borderRadius={15} p={5} gap={3}>
         <Formik
           validationSchema={RecoveryValidation}
           initialValues={RecoveryPasswordInitialValues}
-          onSubmit={() => console.log("submitted baby")}
+          onSubmit={handleSubmit}
         >
           <Form>
             <Flex w={500} flexDir="column">
@@ -44,6 +72,7 @@ const RecoveryPage = () => {
             </Flex>
             <Flex gap={3}>
               <Button
+                isLoading={submitState}
                 type="submit"
                 _hover={{
                   bg: "gray.700",
