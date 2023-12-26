de5f0ec23b73dc39c5d93186a3782481034854ca
akshatsabavat <akshat.sabavat@gmail.com>
2023-12-26 16:13:22 +0530

    Added cursor protection

--- src/pages/memberships/khudabadi-amil-panchayat.tsx
+++ src/pages/memberships/khudabadi-amil-panchayat.tsx
@@ -1,15 +1,36 @@
 import type { NextPage } from "next";
 
-import { Spacer } from "@chakra-ui/react";
+import { Box, Spacer } from "@chakra-ui/react";
 
 import Layout from "~/components/layout";
 import KhudabadiAmilPanchayatMembershipForm from "~/components/forms/kap-membership-form";
+import UserBlockModal from "~/components/authentication/UserBlockModal";
+import { useUserAtom } from "~/lib/atom";
 
 const KhudabadiAmilPanchayatMembershipPage: NextPage = () => {
+  const [{ user }] = useUserAtom();
   return (
     <Layout title="KAP Membership Form">
       <Spacer h="1.5rem" />
-      <KhudabadiAmilPanchayatMembershipForm />
+      <Box position="relative">
+        <Box
+          display={user ? "none" : ""}
+          left="50%"
+          top="50%"
+          transform="translate(-50%,-95%)"
+          zIndex={2}
+          height={100}
+          position="absolute"
+        >
+          <UserBlockModal />
+        </Box>
+        <Box
+          filter={user ? "" : "blur(2px)"}
+          _hover={user ? {} : { cursor: "not-allowed" }}
+        >
+          <KhudabadiAmilPanchayatMembershipForm />
+        </Box>
+      </Box>
       <Spacer h="5rem" />
     </Layout>
   );
