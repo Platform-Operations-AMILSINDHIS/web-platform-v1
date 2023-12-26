c5df42fb0237131e5fb01edf323b561d0b2475ed
akshatsabavat <akshat.sabavat@gmail.com>
2023-12-26 16:21:54 +0530

    Lock safety added to YAC form

--- src/pages/memberships/young-amil-circle.tsx
+++ src/pages/memberships/young-amil-circle.tsx
@@ -1,15 +1,36 @@
 import type { NextPage } from "next";
 
-import { Spacer } from "@chakra-ui/react";
+import { Box, Spacer } from "@chakra-ui/react";
 
 import Layout from "~/components/layout";
 import YoungAmilCircleMembershipForm from "~/components/forms/yac-membership-form";
+import UserBlockModal from "~/components/authentication/UserBlockModal";
+import { useUserAtom } from "~/lib/atom";
 
 const KhudabadiAmilPanchayatMembershipPage: NextPage = () => {
+  const [{ user }] = useUserAtom();
   return (
-    <Layout title="YAC Membership Form">
+    <Layout title="KAP Membership Form">
       <Spacer h="1.5rem" />
-      <YoungAmilCircleMembershipForm />
+      <Box position="relative">
+        <Box
+          display={user ? "none" : ""}
+          left="50%"
+          top="50%"
+          transform="translate(-50%,-95%)"
+          zIndex={2}
+          height={100}
+          position="absolute"
+        >
+          <UserBlockModal />
+        </Box>
+        <Box
+          filter={user ? "" : "blur(2px)"}
+          _hover={user ? {} : { cursor: "not-allowed" }}
+        >
+          <YoungAmilCircleMembershipForm />
+        </Box>
+      </Box>
       <Spacer h="5rem" />
     </Layout>
   );
