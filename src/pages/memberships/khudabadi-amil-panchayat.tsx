867180a85c6608f50a15054716332421db0b342c
akshatsabavat <akshat.sabavat@gmail.com>
2024-02-09 12:11:42 +0530

    Age Blocker added to KAP form

--- src/pages/memberships/khudabadi-amil-panchayat.tsx
+++ src/pages/memberships/khudabadi-amil-panchayat.tsx
@@ -19,8 +19,7 @@ const KhudabadiAmilPanchayatMembershipPage: NextPage = () => {
       <Spacer h="1.5rem" />
       <Box position="relative">
         <Box
-          display="none"
-          // display={user && user.age >= 21 ? "none" : ""}
+          display={user && user.age >= 21 ? "none" : ""}
           left="50%"
           top="50%"
           transform="translate(-50%,-95%)"
@@ -28,10 +27,38 @@ const KhudabadiAmilPanchayatMembershipPage: NextPage = () => {
           height={100}
           position="absolute"
         >
-          {user && user.age <= 21 ? (
-            <UserBlockModal />
+          {user ? (
+            user.age >= 21 ? (
+              <></>
+            ) : (
+              <Flex
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
+                border="1px solid"
+                borderColor="gray.200"
+                padding={5}
+                borderRadius={20}
+                bg={"white"}
+                justify="center"
+                align="center"
+                h={250}
+                w={500}
+              >
+                <Flex gap={2} px={10} align="center" flexDir="column">
+                  <Text fontWeight={600} textAlign="center" fontSize="xl">
+                    Age Requirement not met
+                  </Text>
+                  <Text textAlign="center">
+                    You need to be atleast 21 years of age to be eligible for
+                    KAP member application
+                  </Text>
+                </Flex>
+              </Flex>
+            )
           ) : (
-            <Flex
+            <UserBlockModal />
+          )}
+
+          {/* <Flex
               boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
               border="1px solid"
               borderColor="gray.200"
@@ -53,11 +80,11 @@ const KhudabadiAmilPanchayatMembershipPage: NextPage = () => {
                 </Text>
               </Flex>
             </Flex>
-          )}
+          )} */}
         </Box>
         <Box
-          // filter={user && user.age >= 21 ? "" : "blur(2px)"}
-          // _hover={user && user.age >= 21 ? {} : { cursor: "not-allowed" }}
+          filter={user && user.age >= 21 ? "" : "blur(2px)"}
+          _hover={user && user.age >= 21 ? {} : { cursor: "not-allowed" }}
         >
           <KhudabadiAmilPanchayatMembershipForm />
         </Box>
