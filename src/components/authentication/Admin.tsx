ae99233c5a8c9a5cc7f843c8db3d2c445af60e75
akshatsabavat <akshat.sabavat@gmail.com>
2024-02-29 21:55:31 +0530

    Created: Seperate Admin Auth Component

---
+++ src/components/authentication/Admin.tsx
@@ -0,0 +1,76 @@
+import { Button, Checkbox, Flex, Icon, Text } from "@chakra-ui/react";
+import { AiFillLock } from "react-icons/ai";
+import { Form, Formik } from "formik";
+import { LabelledInput } from "../forms";
+import { AdminLoginValidation } from "~/validations/AuthValidations";
+
+import React from "react";
+import { AdminLoginValues, adminInitialLoginValues } from "~/hooks/useForm";
+import { adminAtomBody } from "~/lib/atom";
+
+interface AdminProps {
+  handleSubmit: (values: AdminLoginValues) => void;
+  submitting: boolean;
+}
+
+const Admin: React.FC<AdminProps> = ({ handleSubmit, submitting }) => {
+  return (
+    <Formik
+      validationSchema={AdminLoginValidation}
+      initialValues={adminInitialLoginValues}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Flex py={4} px={5} gap={6} flexDir="column">
+          <Flex justify="space-between" align="center" w="full">
+            <Text fontWeight={700} fontSize="20px">
+              Admin Login
+            </Text>
+            <Icon pb={1} color="#FF4D00" boxSize={7} as={AiFillLock} />
+          </Flex>
+          <Flex gap={3} w="full" flexDir="column">
+            <LabelledInput
+              label="Enter your email ID"
+              name="email"
+              placeholder="xyz@gmail.com"
+            />
+
+            <LabelledInput
+              label="Enter password"
+              name="password"
+              placeholder="********"
+            />
+
+            <Flex
+              fontWeight={500}
+              w="full"
+              justify="space-between"
+              align="center"
+            >
+              <Flex gap={2}>
+                <Checkbox />
+                <Text>Remember me</Text>
+              </Flex>
+              <Text>Forgot password ?</Text>
+            </Flex>
+          </Flex>
+          <Flex gap={3}>
+            <Button
+              isLoading={submitting}
+              type="submit"
+              _hover={{
+                bg: "gray.700",
+              }}
+              color="white"
+              bg="#0E0E11"
+            >
+              Login
+            </Button>
+          </Flex>
+        </Flex>
+      </Form>
+    </Formik>
+  );
+};
+
+export default Admin;
--- src/pages/admin/auth.tsx
+++ src/pages/admin/auth.tsx
@@ -1,11 +1,9 @@
-import { Box, Button, Checkbox, Flex, Icon, Text } from "@chakra-ui/react";
-import { Form, Formik } from "formik";
+import { Box, Flex } from "@chakra-ui/react";
 import { useState } from "react";
-import { AiFillLock } from "react-icons/ai";
-import { LabelledInput } from "~/components/forms";
+import Admin from "~/components/authentication/Admin";
+
 import { AdminLoginValues, adminInitialLoginValues } from "~/hooks/useForm";
 import { adminAtomBody, useAdminAtom } from "~/lib/atom";
-import { AdminLoginValidation } from "~/validations/AuthValidations";
 
 const AdminAuthPage = () => {
   const [{ admin }, setAdminAtom] = useAdminAtom();
@@ -35,61 +33,7 @@ const AdminAuthPage = () => {
         borderColor="gray.200"
         w={500}
       >
-        <Formik
-          validationSchema={AdminLoginValidation}
-          initialValues={adminInitialLoginValues}
-          onSubmit={handleSubmit}
-        >
-          <Form>
-            <Flex py={4} px={5} gap={6} flexDir="column">
-              <Flex justify="space-between" align="center" w="full">
-                <Text fontWeight={700} fontSize="20px">
-                  Admin Login
-                </Text>
-                <Icon pb={1} color="#FF4D00" boxSize={7} as={AiFillLock} />
-              </Flex>
-              <Flex gap={3} w="full" flexDir="column">
-                <LabelledInput
-                  label="Enter your email ID"
-                  name="email"
-                  placeholder="xyz@gmail.com"
-                />
-
-                <LabelledInput
-                  label="Enter password"
-                  name="password"
-                  placeholder="********"
-                />
-
-                <Flex
-                  fontWeight={500}
-                  w="full"
-                  justify="space-between"
-                  align="center"
-                >
-                  <Flex gap={2}>
-                    <Checkbox />
-                    <Text>Remember me</Text>
-                  </Flex>
-                  <Text>Forgot password ?</Text>
-                </Flex>
-              </Flex>
-              <Flex gap={3}>
-                <Button
-                  isLoading={submitting}
-                  type="submit"
-                  _hover={{
-                    bg: "gray.700",
-                  }}
-                  color="white"
-                  bg="#0E0E11"
-                >
-                  Login
-                </Button>
-              </Flex>
-            </Flex>
-          </Form>
-        </Formik>
+        <Admin handleSubmit={handleSubmit} submitting={submitting} />
       </Box>
     </Flex>
   );
