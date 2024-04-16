fbd2f2de54fd8f4c38eb7e4090046eae10eb587b
akshatsabavat <akshat.sabavat@gmail.com>
2024-04-16 13:03:15 +0530

    Created: Layout for Table components

---
+++ src/layouts/TableLayout.tsx
@@ -0,0 +1,28 @@
+import { Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+
+interface TableLayoutProps {
+  children: React.ReactNode;
+  tableHeaders: string[];
+}
+
+const TableLayout: React.FC<TableLayoutProps> = ({
+  children,
+  tableHeaders,
+}) => {
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            {tableHeaders.map((column_name, index) => {
+              return <Th key={index}>{column_name}</Th>;
+            })}
+          </Tr>
+        </Thead>
+        <Tbody>{children}</Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TableLayout;
