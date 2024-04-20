4b295df0291a204ea768362936d1756602bb361a
akshatsabavat <akshat.sabavat@gmail.com>
2024-04-21 00:30:21 +0530

    Chore: Removed useless imports

--- src/layouts/ProfileViewLayout.tsx
+++ src/layouts/ProfileViewLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { KAPMembershipPDF } from "~/server/pdfs/kap-membership";
 import { KAPMembershipFormValues } from "~/types/forms/membership";
 import dynamic from "next/dynamic";
