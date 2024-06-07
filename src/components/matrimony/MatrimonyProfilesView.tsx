599123715dee93728a79de8c69fe917cd212619c
akshatsabavat <akshat.sabavat@gmail.com>
2024-06-08 03:09:22 +0530

    Added: Files for MatrimonyProfilesView grid component

---
+++ src/components/matrimony/MatrimonyProfilesView.tsx
@@ -0,0 +1,12 @@
+import { MatrimonyProfilesFetchResponse } from "~/types/api";
+
+interface MarrimonyProfilesView {
+  isLoggedIn: boolean;
+  matrimonyProfiles: MatrimonyProfilesFetchResponse[];
+}
+
+const MatrimonyProfilesView = () => {
+  return;
+};
+
+export default MarrimonyProfilesView;
