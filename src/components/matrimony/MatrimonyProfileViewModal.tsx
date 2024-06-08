c50024fd1c2c768b85e8923a1a9553157e4a3b39
akshatsabavat <akshat.sabavat@gmail.com>
2024-06-08 15:34:34 +0530

    Created: component for pofile view modal

---
+++ src/components/matrimony/MatrimonyProfileViewModal.tsx
@@ -0,0 +1,13 @@
+import { MatrimonyFormValues } from "~/types/forms/matrimony";
+
+interface MatrimonyProfileViewModalProps {
+  submission: MatrimonyFormValues;
+}
+
+const MatrimonyProfileViewModal: React.FC<MatrimonyProfileViewModalProps> = ({
+  submission,
+}) => {
+  return <></>;
+};
+
+export default MatrimonyProfileViewModal;
