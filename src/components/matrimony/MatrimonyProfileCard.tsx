2599dd848b19e95d17b6594b37a6e7ae3ec60086
akshatsabavat <akshat.sabavat@gmail.com>
2024-06-08 15:30:30 +0530

    Created:Matrimony profile card component

---
+++ src/components/matrimony/MatrimonyProfileCard.tsx
@@ -0,0 +1,82 @@
+import { Flex, Icon, Text } from "@chakra-ui/react";
+import { MdFemale, MdMale } from "react-icons/md";
+import { MatrimonyFormValues } from "~/types/forms/matrimony";
+import { formatPDFAge } from "~/utils/helper";
+
+interface MatrimonyProfileCardProps {
+  submission: MatrimonyFormValues;
+}
+
+const MatrimonyProfileCard: React.FC<MatrimonyProfileCardProps> = ({
+  submission,
+}) => {
+  return (
+    <Flex
+      p={3}
+      gap={1}
+      borderRadius={10}
+      border="1px solid rgba(31, 41, 55, 0.45)"
+      boxShadow="0px 4px 0px 0px rgba(0, 0, 0, 0.19)"
+      flexDir="column"
+    >
+      <Flex align="center" justify="space-between">
+        <Text pr={6} fontWeight={600} fontSize="xl">
+          {`${submission.personalInfo.firstName} ${submission.personalInfo.lastName},`}{" "}
+          <span
+            style={{
+              fontSize: "medium",
+              color: "rgba(0, 0, 0, 0.51)",
+            }}
+          >{`${formatPDFAge(
+            submission.personalInfo.dateAndTimeOfBirth as unknown
+          )}`}</span>
+        </Text>
+        <Flex
+          borderRadius={5}
+          fontSize="small"
+          bg={submission.personalInfo.gender === "Male" ? "blue.200" : "pink"}
+          px={2}
+          py={0}
+          align="center"
+          gap={1}
+        >
+          <Text fontWeight={600}>{submission.personalInfo.gender}</Text>
+          <Icon
+            boxSize={3}
+            as={submission.personalInfo.gender === "Male" ? MdMale : MdFemale}
+          />
+        </Flex>
+      </Flex>
+      <Flex align="flex-start" flexDir="column">
+        <Text fontWeight={500} fontSize="lg">
+          {submission.personalInfo.occupation}
+        </Text>
+        <Text fontWeight={600} color="gray.500">
+          {submission.personalInfo.placeOfBirth}
+        </Text>
+      </Flex>
+      <Text
+        _hover={{
+          bg: "#1F2937",
+          cursor: "pointer",
+          color: "#FFFF",
+        }}
+        mt={3}
+        w="fit-content"
+        py={1}
+        px={4}
+        border="1px solid"
+        borderColor="#1F2937"
+        color="#1F2937"
+        borderRadius={5}
+        fontSize="small"
+        fontWeight={600}
+        transition="all 0.2s"
+      >
+        View Profile
+      </Text>
+    </Flex>
+  );
+};
+
+export default MatrimonyProfileCard;
--- src/components/matrimony/MatrimonyProfilesView.tsx
+++ src/components/matrimony/MatrimonyProfilesView.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { MatrimonyProfilesFetchResponse } from "~/types/api";
 import { userAtomBody } from "~/types/atoms/users";
 import { formatPDFAge } from "~/utils/helper";
+import MatrimonyProfileCard from "./MatrimonyProfileCard";
 
 interface MatrimonyProfilesViewProps {
   isLoggedIn: boolean;
@@ -37,83 +38,10 @@ const MatrimonyProfilesView: React.FC<MatrimonyProfilesViewProps> = ({
                 .filter((e) => e.user_id !== user?.id)
                 .map((profile, index) => {
                   return (
-                    <Flex
-                      p={3}
-                      gap={1}
-                      borderRadius={10}
-                      border="1px solid rgba(31, 41, 55, 0.45)"
-                      boxShadow="0px 4px 0px 0px rgba(0, 0, 0, 0.19)"
-                      flexDir="column"
+                    <MatrimonyProfileCard
+                      submission={profile.submission}
                       key={index}
-                    >
-                      <Flex align="center" justify="space-between">
-                        <Text pr={6} fontWeight={600} fontSize="xl">
-                          {`${profile.submission.personalInfo.firstName} ${profile.submission.personalInfo.lastName},`}{" "}
-                          <span
-                            style={{
-                              fontSize: "medium",
-                              color: "rgba(0, 0, 0, 0.51)",
-                            }}
-                          >{`${formatPDFAge(
-                            profile.submission.personalInfo
-                              .dateAndTimeOfBirth as unknown
-                          )}`}</span>
-                        </Text>
-                        <Flex
-                          borderRadius={5}
-                          fontSize="small"
-                          bg={
-                            profile.submission.personalInfo.gender === "Male"
-                              ? "blue.200"
-                              : "pink"
-                          }
-                          px={2}
-                          py={0}
-                          align="center"
-                          gap={1}
-                        >
-                          <Text fontWeight={600}>
-                            {profile.submission.personalInfo.gender}
-                          </Text>
-                          <Icon
-                            boxSize={3}
-                            as={
-                              profile.submission.personalInfo.gender === "Male"
-                                ? MdMale
-                                : MdFemale
-                            }
-                          />
-                        </Flex>
-                      </Flex>
-                      <Flex align="flex-start" flexDir="column">
-                        <Text fontWeight={500} fontSize="lg">
-                          {profile.submission.personalInfo.occupation}
-                        </Text>
-                        <Text fontWeight={600} color="gray.500">
-                          {profile.submission.personalInfo.placeOfBirth}
-                        </Text>
-                      </Flex>
-                      <Text
-                        _hover={{
-                          bg: "#1F2937",
-                          cursor: "pointer",
-                          color: "#FFFF",
-                        }}
-                        mt={3}
-                        w="fit-content"
-                        py={1}
-                        px={4}
-                        border="1px solid"
-                        borderColor="#1F2937"
-                        color="#1F2937"
-                        borderRadius={5}
-                        fontSize="small"
-                        fontWeight={600}
-                        transition="all 0.2s"
-                      >
-                        View Profile
-                      </Text>
-                    </Flex>
+                    />
                   );
                 })}
             </Grid>
