e5a4c549bf0dec34be19962d1c8be8e4a2641a2f
akshatsabavat <akshat.sabavat@gmail.com>
2024-09-07 03:39:28 +0530

    Defaulted to previous look

--- src/pages/pastevents/[slug].tsx
+++ src/pages/pastevents/[slug].tsx
@@ -1,60 +1,26 @@
 /* eslint-disable */
-import {
-  Box,
-  Divider,
-  Flex,
-  Spinner,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import LinkButton from "~/components/buttons/LinkButton";
 import ModalGallery from "~/components/events/ModalGallery";
-import PastEventSlider from "~/components/events/PastEventSlider";
 import Layout from "~/components/layout";
-import {
-  PastEventContentType,
-  type PastEventContentTypeQueryQuery,
-  type PastEventDetailQuery,
-} from "~/lib/__generated/sdk";
+import { type PastEventDetailQuery } from "~/lib/__generated/sdk";
 import { client } from "~/lib/client";
 import { convertDateV2 } from "~/utils/helper";
 
 export const getServerSideProps: GetServerSideProps<{
   pastEvent: PastEventDetailQuery;
-  pastEvents: PastEventContentTypeQueryQuery;
 }> = async ({ params }) => {
   const slug = (params?.slug as string) ?? "";
 
   const pastEvent = await client.pastEventDetail({ id: slug });
-  const pastEvents = await client.pastEventContentTypeQuery();
-  return { props: { pastEvent, pastEvents } };
+  return { props: { pastEvent } };
 };
 
 const PastEventDetailPage = ({
   pastEvent: { pastEventContentType },
-  pastEvents,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const router = useRouter();
-  const { slug } = router.query;
-  // useEffect(() => {
-  //   if (pastEventContentType && pastEventContentTypeCollection) {
-  //     const viewedEventTags = pastEventContentType.pastEventSearchTags;
-  //     const recommendations = pastEventContentTypeCollection.items
-  //       .filter(
-  //         (event) =>
-  //           event?.sys.id !== slug &&
-  //           event?.pastEventSearchTags?.some((tag) =>
-  //             viewedEventTags?.includes(tag)
-  //           )
-  //       )
-  //     // console.log(recommendations);
-  //     setReadingRecommendations(recommendations);
-  //   }
-  // }, [pastEventContentType, pastEventContentTypeCollection]);
   return (
     <Layout
       title={
@@ -111,14 +77,16 @@ const PastEventDetailPage = ({
           </Flex>
 
           <Flex mt={8} flexDir="column">
-            <Flex flexDir="column">
-              <Text fontSize={"4xl"} fontWeight={600}>
-                {pastEventContentType?.pastEventLocation}
-              </Text>
-              <Text color="gray.500">
-                {convertDateV2(pastEventContentType?.pastEventDate as Date)}
-              </Text>
-              <Flex my={3} gap={2}>
+            <Flex w="full" justify="space-between" align="flex-start">
+              <Flex flexDir="column">
+                <Text fontSize={"4xl"} fontWeight={600}>
+                  {pastEventContentType?.pastEventLocation}
+                </Text>
+                <Text>
+                  {convertDateV2(pastEventContentType?.pastEventDate as Date)}
+                </Text>
+              </Flex>
+              <Flex gap={2}>
                 <Flex gap={2}>
                   {pastEventContentType?.pastEventSearchTags?.map(
                     (item, index) => {
@@ -154,7 +122,7 @@ const PastEventDetailPage = ({
                 </Text>
               </Flex>
             </Flex>
-            {/* <Divider mt={1} mb={3} border="solid 1px" borderColor="gray.300" /> */}
+            <Divider my={3} border="solid 1px" borderColor="gray.300" />
 
             <Flex gap={3} flexDir="column">
               {/* eslint-disable-next-line */}
