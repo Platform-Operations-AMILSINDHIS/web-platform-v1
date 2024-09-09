37c85a0320366aea4efe9b631522348289d3063c
akshatsabavat <akshat.sabavat@gmail.com>
2024-09-09 14:16:34 +0530

    Defined: Supabase hook

--- src/hooks/useRealTime.tsx
+++ src/hooks/useRealTime.tsx
@@ -1,3 +1,29 @@
-const useRealTime = () => {};
+// src/hooks/useRealTime.ts
+import { useEffect } from "react";
+import supabase from "~/lib/supabase/client";
+
+const useRealTime = (onUpdate: () => Promise<void>, tableName: string) => {
+  useEffect(() => {
+    const channel = supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: `${tableName}`,
+        },
+        (payload) => {
+          console.log("Real-time update:", payload);
+          void onUpdate();
+        }
+      )
+      .subscribe();
+
+    return () => {
+      void supabase.removeChannel(channel);
+    };
+  }, [onUpdate]);
+};
 
 export default useRealTime;
