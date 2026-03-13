1d53ce793e5c8e7a2b47974cb82a5dc28e2a7d54
akshatsabavat <akshat.sabavat@gmail.com>
2026-03-13 00:42:29 -0400

    Completed: SES setup

--- src/server/api/routers/aws.ts
+++ src/server/api/routers/aws.ts
@@ -1,5 +1,7 @@
 // Router for aws resource access + maipulations
 import * as Yup from "yup";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
 import {
   GetObjectCommand,
   PutObjectAclCommand,
@@ -7,7 +9,9 @@ import {
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { adminS3, userS3 } from "~/lib/aws/s3";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+import { sesClient } from "~/lib/aws/ses";
 
 const awsRouter = createTRPCRouter({
   getS3ProfilePicture: publicProcedure
@@ -67,6 +71,55 @@ const awsRouter = createTRPCRouter({
       } catch (err) {
         console.log("Error generating upload URL:", err);
         throw new Error("Could not generate upload URL");
+      }
+    }),
+
+  sendSESEmail: publicProcedure
+    .input(
+      Yup.object({
+        source: Yup.string().required("Source email is required"), // controls sub domain, like yac / kap / matrimony and all that
+        to: Yup.string()
+          .email("Must be a valid email")
+          .required("Recipient email is required"),
+        subject: Yup.string().required("Subject is required"),
+        body: Yup.string().required("Body is required"),
+        html: Yup.string().optional(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const { body, source, subject, to, html } = input;
+      try {
+        // declaring SES command
+        const command = new SendEmailCommand({
+          Source: source, // must match your verified SES domain
+          Destination: {
+            ToAddresses: [to],
+          },
+          Message: {
+            Subject: {
+              Data: subject,
+              Charset: "UTF-8",
+            },
+            Body: {
+              Text: {
+                Data: body,
+                Charset: "UTF-8",
+              },
+              Html: html
+                ? {
+                    Data: html,
+                    Charset: "UTF-8",
+                  }
+                : undefined,
+            },
+          },
+        });
+        return sesClient.send(command);
+      } catch (err) {
+        console.log("Error Sending Email through AWS SES", err);
+        throw new Error(
+          `Something went wrong while sending the email, inform user for ${to} Manually`
+        );
       }
     }),
 });
